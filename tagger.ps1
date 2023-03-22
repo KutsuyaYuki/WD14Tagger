98a162d1380cc28610340d2be4c2ed270e39ec08
青龍聖者@bdsqlsz <865105819@qq.com>
2023-03-22 14:11:53 +0800

    Add files via upload

---
+++ tagger.ps1
@@ -0,0 +1,18 @@
+# tagger script by @bdsqlsz
+
+# Train data path
+$input_img = "./input" # input images path
+$batch_size = 4
+$thresh = 0.35
+
+
+Write-Output "配置完成"
+conda activate wd14tagger
+python tag_images_by_wd14_tagger.py `
+  $input_img `
+  --batch_size=$batch_size `
+  --thresh=$thresh `
+  --caption_extension .txt
+  
+Write-Output "Tagger finished"
+Read-Host | Out-Null ;
